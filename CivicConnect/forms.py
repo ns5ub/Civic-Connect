870a89d400871c22fed937514f189a0730e0aa46
Brooks <bwe3bx@virginia.edu>
2020-10-22 16:05:20 -0400

    User Creation Form

---
+++ CivicConnect/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.models import user
+
+## interests for users to choose from ##
+interests = ["Cybersecurity", "Police Brutality"]
+
+
+class CreateProfile(forms.ModelForm) :
+
+    """ Password creation and validation """
+    password = forms.CharField(widget=forms.PasswordInput)  # ask for password, special widget for a bit more security
+    validate_password = forms.CharField(widget=forms.PasswordInput)  # re-enter password for authentication
+
+    """ Interest selection """
+    user_interests = forms.MultipleChoiceField(choices=interests, required=True)
+
+    """ Date of birth """
+    dob = forms.DateField(required=False)
+
+    """ Location """
+    location = forms.CharField(max_length=200, required=False)
+
+    """ Collect relevant user metadata """
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'user_interests',
+        ]
