8055c909cc989a91263fced485bf52ea1034e60a
ns5ub <ns5ub@virginia.edu>
2020-10-22 11:38:13 -0400

    Added Logout Functionality

--- CivicConnect/urls.py
+++ CivicConnect/urls.py
@@ -8,5 +8,6 @@ app_name = 'CivicConnect'
 urlpatterns = [
     path('', views.home, name='home'),
     path('index/', views.index, name='index'),
+    path("logout/", views.logout_request, name="logout")
     #path('accounts/', include('allauth.urls')),
 ]
--- CivicConnect/views.py
+++ CivicConnect/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 # Create your views here.
 
 def home(request):
-
-
     return render(request, 'CivicConnect/home.html')
 
 def index(request):
     return render(request, 'CivicConnect/index.html')
 
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("CivicConnect:home")
 
