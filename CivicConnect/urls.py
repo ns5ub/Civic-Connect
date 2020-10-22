b8552cf12bc1179eaa6d06bbbad443c596234f09
ns5ub <ns5ub@virginia.edu>
2020-10-22 19:02:27 -0400

    Profile Model + Form
    created a profile model/form at profile url. - currently the user and profile arent hooked together

--- CivicConnect/urls.py
+++ CivicConnect/urls.py
@@ -8,6 +8,8 @@ app_name = 'CivicConnect'
 urlpatterns = [
     path('', views.home, name='home'),
     path('index/', views.index, name='index'),
-    path("logout/", views.logout_request, name="logout")
+    path("logout/", views.logout_request, name="logout"),
+    path("profile/", views.update_profile, name="profile"),
+    path("edit_profile/", views.logout_request, name="edit_profile")
     #path('accounts/', include('allauth.urls')),
 ]
