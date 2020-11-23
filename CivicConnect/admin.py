ed12febb49e7b95bd8ac711fc7cdb9b321d9dfad
ns5ub <ns5ub@virginia.edu>
2020-11-22 22:18:15 -0500

    Fixing Admin Page Views
    Making it easier to see which profile is which and which template is which on admin page

--- CivicConnect/admin.py
+++ CivicConnect/admin.py
@@ -4,6 +4,24 @@ from django.contrib import admin
 
 from .models import Profile, Interest, TemplateSubmission
 
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'address']
+
+
+def mark_approved(modeladmin, request, queryset):
+    queryset.update(approved=True)
+mark_approved.short_description = "Mark selected templates as approved"
+
+
+class TemplateSubmissionAdmin(admin.ModelAdmin):
+    list_display = ['topic', 'template', 'approved']
+    actions = [mark_approved]
+
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Interest)
-admin.site.register(TemplateSubmission)
+admin.site.register(TemplateSubmission, TemplateSubmissionAdmin)
+
+
+
